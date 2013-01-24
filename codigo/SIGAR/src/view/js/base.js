76afe8a26cff09674547bff4b0c8e15398b63e48
Fellype Nascimento <fellypenc@gmail.com>
2013-01-24 03:42:09 -0200

    Adicionada View da Consulta 	*Os botões Editar e Excluir fazem parte de certo modo da view das outras histórias respectivamente.

--- codigo/SIGAR/src/view/js/base.js
+++ codigo/SIGAR/src/view/js/base.js
@@ -4,6 +4,84 @@
 
 $(document).ready(function(){
     
+     $('#endResp').hide();
+    $('#openEndResp').click(function(){
+        $('#endResp').fadeIn('slow');
+    });
+    $('#closeEndResp').click(function(){
+        $('#endResp').hide();
+    });
+    
+    //select all the a tag with name equal to modal
+	$('a[name=modal]').click(function(e) {
+		//Cancel the link behavior
+		e.preventDefault();
+		
+		//Get the A tag
+		var id = $(this).attr('href');
+	
+		//Get the screen height and width
+		var maskHeight = $(document).height();
+		var maskWidth = $(window).width();
+	
+		//Set heigth and width to mask to fill up the whole screen
+		$('#mask').css({'width':maskWidth,'height':maskHeight});
+		
+		//transition effect		
+		$('#mask').fadeIn(1000);	
+		$('#mask').fadeTo("slow",0.8);	
+	
+		//Get the window height and width
+		var winH = $(window).height();
+		var winW = $(window).width();
+              
+		//Set the popup window to center
+		$(id).css('top',  winH/2-$(id).height()/2);
+		$(id).css('left', winW/2-$(id).width()/2);
+	
+		//transition effect
+		$(id).fadeIn(2000); 
+	
+	});
+	
+	//if close button is clicked
+	$('.window .close').click(function (e) {
+		//Cancel the link behavior
+		e.preventDefault();
+		
+		$('#mask').hide();
+		$('.window').hide();
+	});		
+	
+	//if mask is clicked
+	$('#mask').click(function () {
+		$(this).hide();
+		$('.window').hide();
+	});			
+
+	$(window).resize(function () {
+	 
+ 		var box = $('#boxes .window');
+ 
+        //Get the screen height and width
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+      
+        //Set height and width to mask to fill up the whole screen
+        $('#mask').css({'width':maskWidth,'height':maskHeight});
+               
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+
+        //Set the popup window to center
+        box.css('top',  winH/2 - box.height()/2);
+        box.css('left', winW/2 - box.width()/2);
+	 
+	});
+    
+    $('input#id_search').quicksearch('table#table_example tbody tr');
+    
     $('#login input').focus(function(){ 
     if (this.value==this.defaultValue) {
             this.value='';
@@ -20,13 +98,6 @@ $(document).ready(function(){
 
      });
     
-    $('#endResp').hide();
-    $('#openEndResp').click(function(){
-        $('#endResp').fadeIn('slow');
-    });
-    $('#closeEndResp').click(function(){
-        $('#endResp').hide();
-    });
     
     $('#cadEnv').click(function(event){
         alert('Are input fields valid? ' + $('input').isValid());
@@ -44,5 +115,5 @@ $(document).ready(function(){
     $(
         '#inputNome,#inputDataNasc,#inputEmail,#inputTelRes,#inputEscola,#inputEndereco,#inputN,#inputBairro,#inputCidade,#inputUf,#inputCep,#inputNomeResp,#inputDataNascResp,#inputCpf,#inputEmailResp,#inputTelResp'
     ).valid8("Necessário!");
-
+       
 });
