2cf4469bf24b9716528c44a58b04ab84268ecac7
Hebert <Hebert@Hebert-HP>
2013-02-22 23:12:02 -0300

    Criação da AgendamentoCtrl.

---
+++ codigo/SIGAR/src/controller/AgendamentoCtrl.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AgendamentoCtrl
+ *
+ * @author Hebert
+ */
+class AgendamentoCtrl {
+    
+    public function salvarAgendamento(){
+        
+    }
+    
+    
+    
+}
+
+?>
