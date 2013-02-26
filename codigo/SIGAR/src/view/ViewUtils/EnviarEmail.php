2b35f572f88e8f2a761bd54c3cd195f2b297c040
Rafael Ferreira dos Santos <raafael.saantos@gmail.com>
2013-02-26 01:45:30 -0300

    Enviar Email - 50%

--- codigo/SIGAR/src/view/ViewUtils/EnviarEmail.php
+++ codigo/SIGAR/src/view/ViewUtils/EnviarEmail.php
@@ -1,7 +1,6 @@
 <?php
-	$url = $_SERVER['DOCUMENT_ROOT'] . "/SIGAR/codigo/SIGAR/src";
-	require $url.'/view/ValidaSession.php';
-    require_once $url.'/controller/EstatisticaCtrl.php';
+    $url = $_SERVER['DOCUMENT_ROOT'] . "/SIGAR/codigo/SIGAR/src";
+    require $url.'/view/ValidaSession.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,7 +30,6 @@
   <script src="../js/email.js"></script>
   <script src="../js/formCadastroAluno.js"></script>
   <script src="../js/prototype.js"></script>
-  <script src="../js/email.js"></script>
   <link href="../css/tablecloth.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
@@ -52,81 +50,73 @@
                         <div class="spaces">
                             <form action="EnviarEmail.php" name="formEdit" id="formEdit" method="post">
                                 <table border="1" id="table_example">
-                                        <thead>
-                                            <tr>
-                                                <td align="rigth">
-                                                    <select name="fonte" onChange="alterarFonte(this.options[this.selectedIndex].value)">
-                                                        <option value="Arial">Arial</option>
-                                                        <option value="Courier">Courier</option>
-                                                        <option value="Sans Serif">Sans Serif</option>
-                                                        <option value="Tahoma">Tahoma</option>
-                                                        <option value="Times New Roman">Times New Roman</option>
-                                                        <option value="Verdana">Verdana</option>
-                                                    </select>
-                                                    &nbsp;
-                                                    <select name="tamanho" onChange="alterarTamanho(this.options[this.selectedIndex].value)">
-                                                        <option value="1">1</option>
-                                                        <option value="2">2</option>
-                                                        <option value="3">3</option>
-                                                        <option value="4">4</option>
-                                                        <option value="5">5</option>
-                                                        <option value="6">6</option>
-                                                    </select>
-                                                    <img src="../img/enviarEmail/recortar.gif" onClick="recortar()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/copiar.gif" onClick="copiar()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/colar.gif" onClick="colar()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/desfazer.gif" onClick="desfazer()" style="cursor:hand">
-                                                    <br><img src="../img/enviarEmail/refazer.gif" onClick="refazer()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/negrito.gif" onClick="negrito()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/italico.gif" onClick="italico()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/sublinhado.gif" onClick="sublinhado()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/alinhamentoesquerda.gif" onClick="alinharEsquerda();" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/centralizado.gif" onClick="centralizado()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/alinhamentodireita.gif" onClick="alinharDireita()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/numeracao.gif" onClick="numeracao()" style="cursor:hand">
-                                                    <img src="../img/enviarEmail/marcador.gif" onClick="marcadores()" style="cursor:hand">
-                                                </td>
-                                                <td colspan="3"><b><center>SELEÇÃO DE EMAILS</center></b></td>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
+                                    <tr>
+                                       <td align="center" rowspan="3">
+                                                <div id="menu"></div>
+                                                <div id="sample">
+                                                    <div id="myArea1" style="width: 300px; height: 280px; border: 1px solid #000;">
+                                                            Digite aqui o e-mail!
+                                                    </div>
+
+                                                    <script src="nicEdit.js" type="text/javascript"></script>
+                                                    <script>
+
+                                                    var area1, area2;
+
+                                                    function toggleArea1() {
+                                                            if(!area1) {
+                                                                    area1 = new nicEditor({fullPanel : true}).panelInstance('myArea1',{hasPanel : true});
+                                                            } else {
+                                                                    area1.removeInstance('myArea1');
+                                                                    area1 = null;
+                                                            }
+                                                    }
+
+                                                    function addArea2() {
+                                                            area2 = new nicEditor({fullPanel : true}).panelInstance('myArea2');
+                                                    }
+                                                    function removeArea2() {
+                                                            area2.removeInstance('myArea2');
+                                                    }
+
+                                                    bkLib.onDomLoaded(function() { toggleArea1(); });
+
+                                                    </script>	
+                                                </div>
+                                            </td>
+                                            <td colspan="3">
+                                                <b><center>SELEÇÃO DE EMAILS</center></b>
+                                            </td>
+                                        </tr>
                                         <tr>
-                                            <td>&nbsp</td>
                                             <td align="center" width="300"><b><center>Lista Geral</center></b></td>
                                             <td>&nbsp</td>
                                             <td align="center" width="500"><b><center>Selecionados</center></b></td>
                                         </tr>
-                                        <tr>
-                                            <td align="center">
-                                                <!--<iframe id="editor" name="editor" frameborder="0" style="border:1px solid; width: 300px; height:180px"></iframe>-->
-                                                <textarea id="editor" name="editor" frameborder="0" style="border:1px solid; width: 300px; height:180px"></textarea>
-                                            </td>
+                                        <tr align="center">
                                             <form name="frmMalaDireta" method="post" onsubmit="return gravar()" action="EnviarEmail.php">
-                                                    <input type="hidden" id="action" name="action" value="gravar_inscricao" />
-                                                    <input type="hidden" id="tt_disponiveis" name="tt_disponiveis" />
-                                                    <input type="hidden" id="tt_selecionadas" name="tt_selecionadas" />
-                                            <td align="center" width="300">
-                                                    <select id="email_disponivel" multiple="multiple" size="11" style="width: 185px">
-                                                            <?php ?>
-                                                            <option value="lista de emails"> lista de emails </option>
-                                                            	
-                                                    </select>
+                                            <input type="hidden" id="action" name="action" value="gravar_inscricao" />
+                                            <input type="hidden" id="tt_disponiveis" name="tt_disponiveis" />
+                                            <input type="hidden" id="tt_selecionadas" name="tt_selecionadas" />
+                                            <td align="center">
+                                                <select id="email_disponivel" multiple="multiple" size="11" style="width: 220px; height: 300px;">
+                                                    <?php ?>
+                                                    <option value="lista de emails"> lista de emails </option>
+                                                </select>
                                             </td>
                                             <td align="center">
-                                                    <input type="button" class="botao" id="btnIncluirSelecionados" name="btnIncluirSelecionados" onclick="incluirSelecionados()" value=">" style="width: 35px"><br><br>
-                                                    <input type="button" class="botao" id="btnIncluirTodos" name="btnIncluirTodos" onclick="incluirTodos()" value=">>" style="width: 35px" /><br><br>
-                                                    <input type="button" class="botao" id="btnExcluirTodos" name="btnExcluirTodos" onclick="excluirTodos()" value="<<" style="width: 35px" /><br><br>
-                                                    <input type="button" class="botao" id="btnExcluirSelecionados" name="btnExcluirSelecionados" onclick="excluirSelecionados()" value="<" style="width: 35px" />
+                                                <input type="button" class="botao" id="btnIncluirSelecionados" name="btnIncluirSelecionados" onclick="incluirSelecionados()" value=">" style="width: 35px"><br><br>
+                                                <input type="button" class="botao" id="btnIncluirTodos" name="btnIncluirTodos" onclick="incluirTodos()" value=">>" style="width: 35px" /><br><br>
+                                                <input type="button" class="botao" id="btnExcluirTodos" name="btnExcluirTodos" onclick="excluirTodos()" value="<<" style="width: 35px" /><br><br>
+                                                <input type="button" class="botao" id="btnExcluirSelecionados" name="btnExcluirSelecionados" onclick="excluirSelecionados()" value="<" style="width: 35px" />
                                             </td>
-                                            <td align="center" width="500">
-                                                    <select id="email_selecionado" multiple="multiple" size="11" style="width: 180px">
-                                                            <?php  ?>
-                                                                    <option value=""> emais selecionados </option>
-                                                            
-                                                    </select>
+                                            <td align="center">
+                                                <select id="email_selecionado" multiple="multiple" size="11" style="width: 220px; height: 300px;">
+                                                    <?php  ?>
+                                                    <option value=""> emais selecionados </option>
+                                                </select>
                                             </td>
-                                    </tr>
-                                        </tbody>
+                                        </tr>
                                 </table>
                             </form>
                         </div>
